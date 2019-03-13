32ccecaf409db314ea1bf8f68ad834ab6901cebb
manasse228 <javajavaget@yahoo.fr>
2019-03-13 14:24:03 +0100

    add commponent header, footer, catalogue, details, create game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CreateGameComponent } from './create-game/create-game.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { DetailComponent } from './catalogue/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateGameComponent,
+    CatalogueComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
