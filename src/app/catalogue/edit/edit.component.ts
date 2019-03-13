b35aa028e173975d5332c5061c83a571600d20aa
manasse228 <javajavaget@yahoo.fr>
2019-03-13 15:23:48 +0100

    update item from localstorage

--- src/app/catalogue/edit/edit.component.ts
+++ src/app/catalogue/edit/edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Game} from '../../Models/Game';
 
 @Component({
   selector: 'app-edit',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
+  showPanel: false;
+  @Input() game: Game;
   constructor() { }
 
   ngOnInit() {
